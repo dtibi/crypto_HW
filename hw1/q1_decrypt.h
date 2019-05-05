65fc85e205dd4e1be30016da0e58f7f4cf4874a9
Daniel Tibi <cky800@gmail.com>
2019-05-05 21:47:41 +0300

    added decryption attack

--- hw1/q1_decrypt.h
+++ hw1/q1_decrypt.h
@@ -6,7 +6,7 @@
 extern void door_decryption(char* output, char* cipher, int** pkey) {
 	int determinant,inv_det;
 	int i,j,k,n,sum=0;
-	int** adj = alloc_text_to_matrix(key,0);
+	int adj[M][M];
 	int** cipher_text_mat = alloc_text_to_matrix(cipher,1);
 	int** decrypted_text_mat = alloc_text_to_matrix(cipher,1);
 	float** inv_pkey;
@@ -15,18 +15,21 @@ extern void door_decryption(char* output, char* cipher, int** pkey) {
 	inv_pkey = malloc(sizeof(float*)*M);
 	for(i=0;i<M;i++)
 		inv_pkey[i] = malloc(sizeof(float)*M);
-	
+	for(i=0;i<M;i++)
+		for(j=0;j<M;j++)
+			adj[i][j] = key_mat[i][j];
 	//finding determinant of 2x2 matrix
 	determinant = (key_mat[0][0] * key_mat[1][1] - key_mat[0][1] * key_mat[1][0]) % N;
 	inv_det = mod_inverse(determinant,N);
 	
 	if (DEBUG) {
 		print_mat(key_mat,M,M,"2*pkey+pkey^3:");
-		printf("determinant: %f\n", determinant);
+		printf("determinant: %d\n", determinant);
 	}
 	if(determinant == 0) {
-		printf("determinant equal to zero therefore no A^-1 exists!");
-		exit(1);
+		if(DEBUG)printf("determinant equal to zero therefore no A^-1 exists!");
+		strcpy(output, "NULL");
+		return;
 	}
 	adj[0][0] = key_mat[1][1];
 	adj[1][1] = key_mat[0][0];
@@ -34,7 +37,7 @@ extern void door_decryption(char* output, char* cipher, int** pkey) {
 	adj[1][0] = -key_mat[1][0];
 	
 	if(DEBUG){
-		print_mat(adj,M,M,"adjoint matrix");
+		print_mat((int**)adj,M,M,"adjoint matrix");
 		printf("Inverse of matrix is: \n");
 	}
 	for(i = 0; i < M; i++){
@@ -59,7 +62,6 @@ extern void door_decryption(char* output, char* cipher, int** pkey) {
 	
 	for(i=0;i<M;i++)free(key_mat[i]);
 	free(key_mat);
-	free_matrix_to_text(output,adj,M,M);
 	free_matrix_to_text(output,cipher_text_mat,n,M);
 	if(DEBUG)print_mat(decrypted_text_mat, n, M, "dycripted text matrix");
 	free_matrix_to_text(output,decrypted_text_mat,n,M);
